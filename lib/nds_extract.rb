03cc0e7fca1da128744f70521f02e646c7c9dbce
David Knudson <djknudson@icloud.com>
2020-03-02 05:04:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,7 +4,7 @@ require 'pp'
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  # pp director_data
+  pp director_data
   director_gross = 0
 
   director_data[:movies].each do |movie|
